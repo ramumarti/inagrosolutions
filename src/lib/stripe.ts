44f08491ba5caf6b2badbfb2bbec4ef963525d8e
Ramon Marti <ramon@inagrosolutions.com>
2026-05-16 08:29:54 +0200

    fix: update stripe api version to 2024-12-18.acacia

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
-  apiVersion: '2025-01-27' as any,
+  apiVersion: '2024-12-18.acacia' as any,
   appInfo: {
     name: 'Inagrosolutions',
     version: '0.1.0',
