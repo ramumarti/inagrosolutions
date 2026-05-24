ae91414d2dd5290cc1026d374c646c5667fc3b02
Ramon Marti <ramon@inagrosolutions.com>
2026-05-24 19:41:42 +0200

    chore(cache): disable Serwist and replace with custom self-destruct service worker for automated browser cache clearing

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.matchAll();
+    }).then((clients) => {
+      clients.forEach((client) => {
+        if (client.navigate) {
+          client.navigate(client.url);
+        }
+      });
+    })
+  );
+});
